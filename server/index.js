a897ab0ca38286c014d04e30147c854c17125401
naveen <naveenmalineni7@gmail.com>
2021-07-10 10:48:43 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -32,3 +32,24 @@ app.get("/", async (req, res) => {
     const allRegistrations = await db.all(query);
     res.send(allRegistrations)
 })
+
+app.post("/",async (req, res) => {
+    const {Name,Email,Mobile,DOB,JobType} = req.body;
+    const selectUserQuery = `SELECT * FROM registration WHERE Name = '${Name}' and Mobile = '${Mobile}'`;
+    const dbUser = await db.get(selectUserQuery);
+    if (dbUser === undefined) {
+        const query = `INSERT INTO 
+        registration (Name,Email,Mobile,DOB,JobType)
+        VALUES ('${Name}',
+            '${Email}',
+            '${Mobile}',
+            '${DOB}',
+            '${JobType}')`;
+        const response = await db.run(query);
+        response.send("user created successfully");
+    }else{
+        res.status(400);
+        res.send("User already exists");
+
+    }
+})
